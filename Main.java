6b7158c0a86fc72f7a429c7e8b1c76f1d3df09d3
eliizquierdo <eizquierdo@docente.ceibal.edu.uy>
2022-07-17 19:53:29 -0300

    Agregue un syso usando VS Code

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner entrada=new Scanner(System.in);
-		
-		System.out.println("Hello Programación2 ");
+		// Scanner entrada=new Scanner(System.in);
 
-  }
+		System.out.println("Hello Programación2 desce VS Code ");
+
+	}
 }
